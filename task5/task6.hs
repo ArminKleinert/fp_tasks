7d751ea6b862f9906693bff1138623915a6e62f7
ArminKleinert <armin.kleinert@t-online.de>
2021-01-03 15:50:21 +0100

    .

--- task5/task6.hs
+++ task5/task6.hs
@@ -13,6 +13,12 @@ Y = (w - (B/255))/w
 K = 1 - w
 -}
 
+type RGBF = (Integer, Integer, Integer)
+type CMYKF = (Double,Double,Double,Double)
+
+data Color = RGB RGBF | CMYK CMYKF
+            deriving Show
+
 maxDouble :: Double -> Double -> Double -> Double
 maxDouble d0 d1 d2 | d0 > d1 && d0 > d2 = d0
                    | d1 > d0 && d1 > d2 = d1
@@ -21,20 +27,20 @@ maxDouble d0 d1 d2 | d0 > d1 && d0 > d2 = d0
 selfOrZeroIfNaN :: Double -> Double
 selfOrZeroIfNaN n = if n /= n then 0 else n
 
-rgb2cmyk :: (Int,Int,Int) -> (Double,Double,Double,Double)
-rgb2cmyk (r,g,b) = let rf = (fromIntegral r) / 255.0
-                       gf = (fromIntegral g) / 255.0
-                       bf = (fromIntegral b) / 255.0
-                       w = maxDouble rf gf bf
-                       c = selfOrZeroIfNaN ((w - rf) / w)
-                       m = selfOrZeroIfNaN ((w - gf) / w)
-                       y = selfOrZeroIfNaN ((w - bf) / w)
-                       k = 1.0 - w
-                   in (c,m,y,k)
+rgb2cmyk :: Color -> Color
+rgb2cmyk (RGB (r,g,b)) = let rf = (fromIntegral r) / 255.0
+                             gf = (fromIntegral g) / 255.0
+                             bf = (fromIntegral b) / 255.0
+                             w = maxDouble rf gf bf
+                             c = selfOrZeroIfNaN ((w - rf) / w)
+                             m = selfOrZeroIfNaN ((w - gf) / w)
+                             y = selfOrZeroIfNaN ((w - bf) / w)
+                             k = 1.0 - w
+                         in CMYK (c,m,y,k)
 
 -- Tests
 
 test :: IO ()
 test = putStrLn ("rgb2cmyk..." ++
-                 "\n (0,0,0): " ++ (show (rgb2cmyk (0,0,0))) ++
-                 "\n (255,255,255): " ++ (show (rgb2cmyk (255,255,255))))
+                 "\n (0,0,0): " ++ (show (rgb2cmyk (RGB (0,0,0)))) ++
+                 "\n (255,255,255): " ++ (show (rgb2cmyk (RGB (255,255,255)))))
--- task5/task7.hs
+++ task5/task7.hs
@@ -6,41 +6,26 @@ Abgabe von Armin Kleinert und Anna Sophie Pipperr
 -- A7
 
 data RootNum = RootNum Int Int
-  deriving (Show, Eq, Ord, Num)
 
-instance Num a => Num (RootNum Int Int) where
-   (RootNum a b) + (RootNum c d) = RootNum (a+c) (b+d)
-   (RootNum a b) * (RootNum c d) = RootNum (a*c) (b*d)
-   (RootNum a b) - (RootNum c d) = RootNum (a-c) (b-d)
-   abs    (RootNum a b) = RootNum (abs a)    (abs b)
-   signum (RootNum a b) = RootNum (signum a) (signum b)
-   fromInteger i = RootNum (fromInteger i) (fromInteger i)
+instance Show RootNum where
+          show (RootNum a b) = show a ++ "+" ++ show b ++ "sqrt(2)"
 
-
-denominator :: RootNum -> Int
-denominator (RootNum n d) = d
-
-numerator :: RootNum -> Int
-numerator (RootNum n d) = n
-
-simplify :: RootNum -> RootNum
-simplify (RootNum _p _q) = RootNum p2 q2
-  where (p1,q1) = if (_q < 0) then (_p * (-1), _q * (-1)) else (_p, _q)
-        denom = gcd _p _q
-        p2 = div p1 denom
-        q2 = div q1 denom
-
--- (+) :: RootNum -> RootNum -> RootNum
--- (+) (RootNum n1 d1) (RootNum n2 d2) = simplify (RootNum n3 d3)
---   where n3 = ((n1 * d2) + (d1 * n2))
---         d3 = d2
+instance Num RootNum where
+          (RootNum a1 b1) + (RootNum a2 b2) = (RootNum (a1+a2) (b1+b2))
+          (RootNum a1 b1) - (RootNum a2 b2) = (RootNum (a1-a2) (b1-b2))
+          (RootNum a1 b1) * (RootNum a2 b2) = (RootNum ((a1*a2)+(b1*b2*2)) (a1*b2+b1*a2))
 
 
+getValue :: RootNum -> Float
+getValue (RootNum a b) = (fromIntegral a)+(fromIntegral b)*(sqrt (fromIntegral 2))
+
+-- Test 
+
+test :: IO ()
+test = putStrLn ("(3+2*(sqrt(2))) * (2+1*(sqrt(2))): " ++ (show ((3+2*(sqrt(2))) * (2+1*(sqrt(2))))) ++
+                 "\n(RootNum 3 2) * (RootNum 2 1): " ++ (show ((RootNum 3 2) * (RootNum 2 1))) ++
+                 "\ngetValue ((RootNum 3 2) * (RootNum 2 1))" ++ (show (getValue ((RootNum 3 2) * (RootNum 2 1)))))
 
 
-
-
-
-
-
+                 
 
